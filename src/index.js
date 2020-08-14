c3ccf6f968159291c0378abd71f8b301f0f1c74a
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-08-14 22:49:24 +0200

    🏎️ first connection to discord successful, implement discord api token as env variable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('hi');
+import dotenv from 'dotenv';
+import Discord from 'discord.js';
+
+const env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+  dotenv.config({ path: './src/secret/.env' });
+}
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+client.on('message', (message) => {
+  if (message.content === '!ping') {
+    // send back "Pong." to the channel the message was sent in
+    message.channel.send('Pong.');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
