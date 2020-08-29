5c9baf57f26f36ca8e53f0035f70f8bff0a330bf
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-08-29 15:10:23 +0200

    🏎️ [feature] include a warning to users inside the help command

--- src/discord/commands/commands/help.js
+++ src/discord/commands/commands/help.js
@@ -14,7 +14,9 @@ export default {
     embed
       .setColor('#0099ff')
       .setTitle('Solana Discord Tipping Bot')
-      .setDescription('Hi! I\'m the Solana Discord Tipping Bot.\nYou can use me to send and receive SOL through discord!')
+      .setDescription('Hi! I\'m the Solana Discord Tipping Bot.\nYou can use me to send and receive SOL through discord!\n🚨Warning🚨 This wallet' +
+          ' is not built to be the most secure wallet ever and provides no guarantees for 100% bug-free code and takes no responsibility for the loss' +
+          ' of funds. Please use the bot to create a new wallet and only transfer small amounts to it.')
       .addFields(...commandInstructions);
 
     message.channel.send(embed);
