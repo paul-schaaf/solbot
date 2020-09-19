27e9e4a62983f25303769697cdcadabe0037b403
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-09-19 02:15:41 +0200

    added info to !help about storing private keys

--- src/discord/commands/commands/help.js
+++ src/discord/commands/commands/help.js
@@ -16,10 +16,12 @@ export default {
       .setTitle('Solbot')
       .setDescription('Hi! I\'m Solbot. 😁\nYou can use me to send and receive SOL through discord!\n🚨 Warning 🚨 This wallet'
           + ' is not built to be the most secure wallet ever and provides no guarantees for 100% bug-free code and takes no responsibility for the loss'
-          + ' of funds. Please use the bot to create a new wallet to be used in discord and only transfer small amounts to it.\n'
+          + ' of funds. Because of the way discord bots work, we need to store your private keys server side while you\'re logged in\n'
+          + 'Please use the bot to create a new wallet to be used in discord and only transfer small amounts to it.\n'
           + 'ℹ️ To keep your wallet info safe, only the "!send" command is available in a public discord channel. DM the bot for all other commands. ℹ️️'
           + '\nIf you\'re interested in the source code, go here: https://github.com/paul-schaaf/solbot')
-      .addFields(...commandInstructions);
+      .addFields(...commandInstructions)
+      .setFooter('🚨 Repeated Warning 🚨 We have to save your private keys server side while you\'re logged in. Do not use your real wallet for this bot.');
 
     message.channel.send(embed);
   },
